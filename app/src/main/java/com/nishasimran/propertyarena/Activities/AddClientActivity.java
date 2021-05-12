5ece44da505d2f557b39a3bffbfd828c52dd39bb
Simran Sharma <simran.s.sharma00@gmail.com>
2021-05-12 12:43:03 +0530

    Added activity for adding a client

---
+++ app/src/main/java/com/nishasimran/propertyarena/Activities/AddClientActivity.java
@@ -0,0 +1,16 @@
+package com.nishasimran.propertyarena.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nishasimran.propertyarena.R;
+
+public class AddClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_client);
+    }
+}
