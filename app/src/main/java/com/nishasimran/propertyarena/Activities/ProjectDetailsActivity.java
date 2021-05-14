bdd41da28f7002cdd04045d86643c6f478033433
Simran Sharma <simran.s.sharma00@gmail.com>
2021-05-14 15:23:00 +0530

    Added an activity for showing full details of a particular project

---
+++ app/src/main/java/com/nishasimran/propertyarena/Activities/ProjectDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.nishasimran.propertyarena.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nishasimran.propertyarena.R;
+
+public class ProjectDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_details);
+    }
+}
